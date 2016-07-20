a7811f3784a831cf80609048f6694b048d0e303e
jian ren <renjiandreamore@users.noreply.github.com>
2016-07-20 15:51:16 -0400

    pinning Test

--- pinnTest.java
+++ pinnTest.java
@@ -3,7 +3,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.Mockito; 
 import org.mockito.MockitoAnnotations;
 
 
